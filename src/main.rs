997235e46c94d7032feb96279c1ed87303651fe7
oscarbrehier <oscarbrehier@users.noreply.github.com>
2025-10-16 20:32:22 +0000

    backup env, starting dry_run implementation

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
 use clap::{Parser, Subcommand};
 use std::collections::HashMap;
 use std::fs;
-use std::fs::OpenOptions;
-use std::hash::Hash;
-use std::io::Write;
 
 #[derive(Parser)]
 #[command(name = "envy", version, about = "Format and validate .env files")]
@@ -33,18 +30,24 @@ enum Commands {
     Format {
         path: String,
         #[arg(short, long, default_value = "keep-first")]
-        dupes: String
-    },
-    Validate {
-        path: String,
-        #[arg(short, long, action = clap::ArgAction::SetTrue)]
-        error: bool,
+        dupes: String,
+        #[arg(short = 'n', long)]
+        dry_run: bool,
     },
     Sort {
         path: String,
         #[arg(short, long, default_value = "group")]
         method: String,
-    }
+        #[arg(short = 'n', long)]
+        dry_run: bool,
+    },
+    Validate {
+        path: String,
+        #[arg(short, long, action = clap::ArgAction::SetTrue)]
+        check_required: bool,
+        #[arg(short, long, action = clap::ArgAction::SetTrue)]
+        error: bool,
+    },
 }
 
 fn parse_env_file(path: &str) -> Result<Vec<Line>, std::io::Error> {
@@ -69,10 +72,10 @@ fn parse_env_file(path: &str) -> Result<Vec<Line>, std::io::Error> {
 
         if let Some((key, value)) = trimmed.split_once('=') {
 
-            let key = key.trim().to_string();
-            let value = value.trim().to_string();
+            let key = key.to_string();
+            let value = value.to_string();
 
-            if key.is_empty() || value.is_empty() {
+            if key.trim().is_empty() || value.trim().is_empty() {
                 lines.push(Line::Invalid { content: trimmed.to_string(), line: line_num });
                 continue ;
             }
@@ -98,7 +101,24 @@ fn parse_env_file(path: &str) -> Result<Vec<Line>, std::io::Error> {
 
 }
 
-fn format_env_file(path: &str, dupes: &str) -> Result<String, std::io::Error> {
+fn get_keys(parsed_lines: &[Line]) -> Vec<String> {
+
+    parsed_lines
+        .iter()
+        .filter_map(|line| {
+
+            if let Line::KeyValue { key, .. } = line {
+                Some(key.clone())
+            } else {
+                None
+            }
+
+        })
+        .collect()
+
+}
+
+fn format_env_file(path: &str, dupes: &str, dry_run: bool) -> Result<String, std::io::Error> {
 
     let mut lines = parse_env_file(path).expect("failed to parse file");
     let mut formatted: Vec<String> = Vec::new();
@@ -111,6 +131,9 @@ fn format_env_file(path: &str, dupes: &str) -> Result<String, std::io::Error> {
         lines.reverse();
     }
 
+    let lines_num = lines.iter().enumerate().len();
+    let width = lines_num.to_string().len();
+
     for line in lines {
 
         match line {
@@ -121,7 +144,31 @@ fn format_env_file(path: &str, dupes: &str) -> Result<String, std::io::Error> {
 
                 if keys.contains(&key) {
 
+                    if dry_run {
+                        println!("{:>width$} | - {}={}", line, key, value, width = width);
+                        println!("{:>width$} | + (line removed - duplicate key)\n", "", width = width);
+                        continue ;
+                    }
+
                     eprintln!("Removed duplicate key: {} (line {})", key, line);
+                    continue ;
+
+                }
+
+                if key.contains(char::is_whitespace) || value.contains(char::is_whitespace) {
+
+                    if dry_run {
+                        println!("{:>width$} | - {}={}", line, key, value, width = width);
+                        println!("{:>width$} | + {}={}", "", key.trim(), value.trim(), width = width);
+                        println!("{:>width$} | (remove extra spaces)\n", "", width = width);
+                        continue ;
+                    }
+
+                    formatted.push(format!("{}={}\n", key.trim(), value.trim()));
+                    formatted_lines += 1;
+
+                    keys.push(key);
+
                     continue ;
 
                 }
@@ -133,6 +180,13 @@ fn format_env_file(path: &str, dupes: &str) -> Result<String, std::io::Error> {
 
             },
             Line::Invalid { content, line } => {
+
+                if dry_run {
+                    println!("{:>width$} | - INVALID LINE", line, width = width);
+                    println!("{:>width$} | + (line removed - invalid syntax)\n", "", width = width);
+                    continue ;
+                }
+
                 eprintln!("Skipping invalid line: {} (line {})", content, line);
                 skipped_invalid += 1;
             }
@@ -140,7 +194,7 @@ fn format_env_file(path: &str, dupes: &str) -> Result<String, std::io::Error> {
         }
 
     }
-
+    
     if dupes == "keep-last" {
         formatted.reverse();
     }
@@ -157,11 +211,33 @@ fn format_env_file(path: &str, dupes: &str) -> Result<String, std::io::Error> {
 
 }
 
-fn validate(path: &str, error_mode: bool) -> bool {
+fn check_required_keys(parsed_file: &Vec<Line>) {
+
+    let example_lines = parse_env_file(".env.example").expect("failed to parse file");
+
+    let required_keys: Vec<String> = get_keys(&example_lines);
+    let keys: Vec<String> = get_keys(&parsed_file);
+
+    for rkey in required_keys {
+
+        if !keys.contains(&rkey) {
+            println!("Missing required key {}", rkey);
+        }
+
+    }
+
+}
+
+fn validate(path: &str, check_required: bool, error_mode: bool) -> bool {
 
     let lines = parse_env_file(path).expect("failed to parse file");
+
     let mut map: HashMap<String, Vec<(String, usize)>> = HashMap::new();
     let mut has_error = false;
+    
+    if check_required {
+        check_required_keys(&lines);
+    }
 
     for line in &lines {
 
@@ -227,36 +303,64 @@ fn sort(path: &str, method: &str) -> Result<String, std::io::Error> {
     let lines: Vec<Line> = parse_env_file(path).expect("failed to parse file");
 
     let mut output = String::new();
-    let mut grouped_keys: HashMap<String, Vec<(String, String)>> = HashMap::new();
+   
+    if method == "alpha" {
 
-    for line in &lines {
+        let mut key_list = Vec::<Vec<String>>::new();
 
-        if let Line::KeyValue { key, value, .. } = line {
+        for line in &lines {
 
-            let prefix = key
-                .split('_').next().unwrap_or("").to_string();
-
-            grouped_keys.entry(prefix)
-                .or_insert_with(Vec::new)
-                .push((key.clone(), value.clone()));
+            if let Line::KeyValue { key, value, .. } = line {
+                key_list.push(vec![key.to_string(), value.to_string()]);
+            }
 
         }
 
-    }
-
-    for values in grouped_keys.values_mut() {
-        values.sort_by(|a, b| a.0.cmp(&b.0));
-    }
-    
-    for (prefix, entries) in grouped_keys {
-
-        output.push_str(&format!("# {}\n", prefix.to_uppercase()));
-
-        for (key, value) in entries {
-            output.push_str(&format!("{}={}\n", key, value));
+        key_list.sort_by(|a, b| a[0].cmp(&b[0]));
+        
+        for kv in key_list {
+            output.push_str(&format!("{}={}\n", kv[0], kv[1]));
         }
 
         output.push('\n');
+        
+    } else {
+
+        let mut grouped_keys: HashMap<String, Vec<(String, String)>> = HashMap::new();
+
+        for line in &lines {
+
+            if let Line::KeyValue { key, value, .. } = line {
+
+                let prefix = if key.split('_').nth(1).is_none() {
+                    String::from("MISC")
+                } else {
+                    key.split('_').next().unwrap_or("").to_string()
+                };
+
+                grouped_keys.entry(prefix)
+                    .or_insert_with(Vec::new)
+                    .push((key.clone(), value.clone()));
+
+            }
+
+        }
+
+        for values in grouped_keys.values_mut() {
+            values.sort_by(|a, b| a.0.cmp(&b.0));
+        }
+        
+        for (prefix, entries) in grouped_keys {
+
+            output.push_str(&format!("# {}\n", prefix.to_uppercase()));
+
+            for (key, value) in entries {
+                output.push_str(&format!("{}={}\n", key, value));
+            }
+
+            output.push('\n');
+
+        }
 
     }
 
@@ -264,15 +368,28 @@ fn sort(path: &str, method: &str) -> Result<String, std::io::Error> {
 
 }
 
-fn write_to_file(path: &str, content: &str) {
+fn backup_file(path: &str) -> Result<(), std::io::Error> {
 
-     let mut file = OpenOptions::new()
-        .write(true)
-        .truncate(true)
-        .open(&path)
-        .expect("cannot open file");
+    let content = fs::read_to_string(path)?;
+    let backup_path = format!("{}.bak", path);
 
-    file.write(content.as_bytes()).expect("write failed");
+    fs::write(&backup_path, content)?;
+
+    println!("Backup of {} located at {}", path, &backup_path);
+
+    Ok(())
+
+}
+
+fn write_to_file(path: &str, content: &str) -> Result<(), std::io::Error> {
+    
+    let temp_path = format!("{}.tmp", path);
+
+    backup_file(path).expect("env backup failed");
+    fs::write(&temp_path, content)?;
+    fs::rename(&temp_path, path)?;
+
+    Ok(())
 
 }
 
@@ -281,28 +398,30 @@ fn main() {
 
     match cli.command {
 
-        Commands::Format { path, dupes } => {
+        Commands::Format { path, dupes, dry_run } => {
 
             println!("Formatting file: {}", path);
 
-            let formatted_file = format_env_file(&path, &dupes).expect("formatting failed");
+            let formatted_file = format_env_file(&path, &dupes, dry_run).expect("formatting failed");
 
-            write_to_file(&path, &formatted_file);
+            if !dry_run {
+                write_to_file(&path, &formatted_file).expect("failed to write");
+            }
 
         }
-        Commands::Validate { path, error } => {
+        Commands::Validate { path, check_required, error } => {
 
-            if !validate(&path, error) {
+            if !validate(&path, check_required, error) {
                 std::process::exit(1);
             }
 
         }
 
-        Commands::Sort { path, method } => {
+        Commands::Sort { path, method, .. } => {
 
             let sorted_content = sort(&path, &method).expect("sorting failed");
-            
-            write_to_file(&path, &sorted_content);
+            write_to_file(&path, &sorted_content).expect("failed to write");
+
 
         }
 
